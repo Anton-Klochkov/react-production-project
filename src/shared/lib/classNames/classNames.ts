a926798624d0cdc8b81b1fee752e58fb63150910
Klochkov Anton <anton.klochkov@neuro-city.ru>
2022-10-06 03:38:59 +0300

    add jest and first test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,15 +1,15 @@
 export type Mods = Record<string, boolean | string>;
 
-export const classNames = (
+export function classNames(
   cls: string,
   mods: Mods = {},
   additional: string[] = [],
-): string => {
+): string {
   return [
     cls,
     ...additional.filter(Boolean),
     ...Object.entries(mods)
-      .filter(([className, value]) => !!value)
+      .filter(([_, value]) => !!value)
       .map(([className]) => className),
   ].join(' ');
-};
+}
